f1a41a348698391faea881f6974e518cfacf2144
Alex Santos <devalexsantos@gmail.com>
2024-01-19 06:11:28 -0300

    fix: list-all controller refactory

--- src/http/controllers/games/list-all.ts
+++ src/http/controllers/games/list-all.ts
@@ -1,10 +1,10 @@
 import { GamesRepository } from '../../repositories/prisma-games-repository'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
-export async function listAll(request: FastifyRequest, reply: FastifyReply) {
+export async function listAll(_: FastifyRequest, reply: FastifyReply) {
   try {
-    const listGames = new GamesRepository()
-    const games = await listGames.get()
+    const gamesRepository = new GamesRepository()
+    const games = await gamesRepository.get()
 
     return reply.status(200).send(games)
   } catch (err) {
